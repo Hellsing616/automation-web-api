e0d322b02f6bca2cfb2de736de627b70d58c5394
Hellsing616 <alesilva6162gmail.com>
2026-08-05 15:28:08 -0300

    feat: criar automacao aoi cenarios

--- cypress/e2e/features/api/trello/trello.feature
+++ cypress/e2e/features/api/trello/trello.feature
@@ -1,11 +1,18 @@
 Feature: Consulta Action Trello
 
   Scenario: Consultar uma action existente
-
     Given que possuo o endpoint da Action do Trello
-
     When realizo uma requisição GET da Action
-
     Then o serviço deve retornar status code 200
-
     And deve exibir o nome da lista
+
+  Scenario: Consultar Action existente
+    Given que possuo o endpoint da Action do Trello
+    When realizo uma requisição GET da Action
+    Then o serviço deve retornar status code 200
+    And deve retornar os dados principais da Action
+    And deve retornar os dados da lista
+    And deve retornar os dados do board
+    And deve retornar os dados do card
+    And deve retornar os dados do membro criador
+    And o tipo da Action deve ser "updateCard"
--- cypress/support/step_definitions/services/trello.steps.js
+++ cypress/support/step_definitions/services/trello.steps.js
@@ -32,3 +32,69 @@ Then('deve exibir o nome da lista', () => {
     expect(response.body.data.list.name).to.not.be.empty;
 
 });
+
+Then('deve retornar os dados principais da Action', () => {
+
+    expect(response.body).to.have.property('id');
+    expect(response.body).to.have.property('type');
+    expect(response.body).to.have.property('date');
+    expect(response.body).to.have.property('data');
+    expect(response.body).to.have.property('memberCreator');
+
+});
+
+Then('deve retornar os dados da lista', () => {
+
+    expect(response.body.data.list).to.have.property('id');
+    expect(response.body.data.list).to.have.property('name');
+
+    expect(response.body.data.list.id).to.be.a('string');
+    expect(response.body.data.list.name).to.be.a('string').and.not.be.empty;
+
+});
+
+Then('deve retornar os dados do board', () => {
+
+    expect(response.body.data.board).to.have.property('id');
+    expect(response.body.data.board).to.have.property('name');
+    expect(response.body.data.board).to.have.property('shortLink');
+
+    expect(response.body.data.board.id).to.be.a('string');
+    expect(response.body.data.board.name).to.be.a('string').and.not.be.empty;
+    expect(response.body.data.board.shortLink).to.be.a('string').and.not.be.empty;
+
+});
+
+Then('deve retornar os dados do card', () => {
+
+    expect(response.body.data.card).to.have.property('id');
+    expect(response.body.data.card).to.have.property('idShort');
+    expect(response.body.data.card).to.have.property('name');
+    expect(response.body.data.card).to.have.property('shortLink');
+    expect(response.body.data.card).to.have.property('due');
+
+    expect(response.body.data.card.id).to.be.a('string');
+    expect(response.body.data.card.idShort).to.be.a('number');
+    expect(response.body.data.card.name).to.be.a('string').and.not.be.empty;
+    expect(response.body.data.card.shortLink).to.be.a('string').and.not.be.empty;
+    expect(response.body.data.card.due).to.be.a('string');
+
+});
+
+Then('deve retornar os dados do membro criador', () => {
+
+    expect(response.body.memberCreator).to.have.property('id');
+    expect(response.body.memberCreator).to.have.property('fullName');
+    expect(response.body.memberCreator).to.have.property('username');
+
+    expect(response.body.memberCreator.id).to.be.a('string');
+    expect(response.body.memberCreator.fullName).to.be.a('string').and.not.be.empty;
+    expect(response.body.memberCreator.username).to.be.a('string').and.not.be.empty;
+
+});
+
+Then('o tipo da Action deve ser {string}', (tipo) => {
+
+    expect(response.body.type).to.eq(tipo);
+
+});
